1706eea05826ea7abc03926f63eef9c3ab8b39c9
ian miller <ianmm17@yahoo.com>
2022-01-06 20:27:05 -0600

    adds doWeMatch function with test

--- JSPractice.js
+++ JSPractice.js
@@ -14,4 +14,15 @@ function collectionsIntArray(nums) {
     }
     return newSlice
 }
-module.exports = {sum, doubleInt, collectionsIntArray}
+function doWeMatch(array1, array2) {
+    if (array1.length != array2.length) {
+        return false
+    }
+    for (let i = 0; i < array1.length; i++) {
+        if (array1[i] != array2[i]) {
+            return false
+        }
+    }
+    return true
+}
+module.exports = {sum, doubleInt, collectionsIntArray, doWeMatch}
