ab15c9023b7a83e0e372c400d8004d47822ea17f
ian miller <ianmm17@yahoo.com>
2022-01-12 20:37:43 -0600

    Lecture 1 studio

---
+++ studios/lecture1.js
@@ -0,0 +1,13 @@
+// Declare and Initialize Variables
+const date = "Monday 2019-03-18"
+const time = "10:05:34 AM"
+const astronautCount = 7
+const astronautStatus = "ready"
+const averageAstronautMassKg = 80.7
+const crewMassKg = astronautCount * averageAstronautMassKg
+const fuelMassKg = 760000
+const shuttleMassKg = 74842.31
+const totalMassKg = crewMassKg + fuelMassKg + shuttleMassKg
+const fuelTempCelsius = -225
+const fuelLevel = 100
+const weatherStatus = "clear"
