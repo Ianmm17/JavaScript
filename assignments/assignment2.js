6c3a2b22282e9c3ed1637ee93e911644508564b4
Miller <milleri@wwt.com>
2022-02-11 14:57:23 -0600

    WIP

--- assignments/assignment2.js
+++ assignments/assignment2.js
@@ -10,33 +10,7 @@ const oldPointStructure = {
     10: ['Q', 'Z']
 };
 
-const newPointStructure = {
-    a: 1,
-    e: 1,
-    i: 1,
-    o: 1,
-    u: 1,
-    l: 1,
-    n: 1,
-    r: 1,
-    s: 1,
-    t: 1,
-    d: 2,
-    g: 2,
-    b: 3,
-    c: 3,
-    m: 3,
-    p: 3,
-    f: 4,
-    h: 4,
-    v: 4,
-    w: 4,
-    y: 4,
-    k: 5,
-    j: 8,
-    x: 8,
-    q: 10,
-    z: 10}
+const newPointStructure = transform(oldPointStructure)
 
 function oldScrabbleScorer(word) {
     word = word.toUpperCase();
@@ -144,8 +118,8 @@ function scorerPrompt() {
 }
 
 function transform(oldPointStructure) {
-    oldPointStructure = newPointStructure
-    return oldPointStructure
+    for (const item in oldPointStructure) {
+    }
 };
 
 
